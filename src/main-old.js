f5408ce67fe0df4d62dfb6cfe81b34b3b06a54f6
Bastien Sarolea <bastien.sarolea@gmail.com>
2023-11-17 15:02:31 +0100

    1

---
+++ src/main-old.js
@@ -0,0 +1,126 @@
+import {
+    bootstrapCameraKit,
+    createMediaStreamSource,
+    Transform2D,
+    //Injectable,
+    //RemoteApiService,
+    //RemoteApiServices,
+    //RemoteApiRequest,
+    //RemoteApiRequestHandler,
+    //RemoteApiStatus,
+    //remoteApiServicesFactory
+
+} from '@snap/camera-kit'
+
+(async function () {
+
+
+    //DAMS
+    const damsService = {
+        apiSpecId: "87e3aee3-0a82-4fbd-8d71-b4534c79704c",
+
+        getRequestHandler(request) {
+            if (request.endpointId !== "codemail") return;
+
+
+            return (reply) => {
+                fetch("https://bouygues-404412.lm.r.appspot.com/codemail?code=x&mail=y", {
+                    headers: {
+                        Accept: "application/json",
+                    },
+                })
+                    .then((res) => res.text())
+                    .then((res) =>
+                        reply({
+                            status: "success",
+                            metadata: {},
+                            body: new TextEncoder().encode(res),
+                        })
+                    );
+            };
+        },
+    };
+
+    // const damsService2 = {
+    //     apiSpecId: "87e3aee3-0a82-4fbd-8d71-b4534c79704c",
+
+    //     getRequestHandler(request) {
+    //         if (request.endpointId !== "codemail") return;
+
+    //         return (reply) => {
+    //             fetch("https://bouygues-404412.lm.r.appspot.com/codemail", {
+    //                 headers: {
+    //                     Accept: "application/json",
+    //                 },
+    //             })
+    //                 .then((res) => res.text())
+    //                 .then((res) =>
+    //                     reply({
+    //                         status: "success",
+    //                         metadata: {},
+    //                         body: new TextEncoder().encode(res),
+    //                     })
+    //                 );
+    //         };
+    //     },
+    // };
+    //
+
+
+    //var expressService = await RemoteApiService({ apiSpecId: '87e3aee3-0a82-4fbd-8d71-b4534c79704c' });
+
+
+
+
+    var cameraKit = await bootstrapCameraKit({ apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjk4NDEyNDI1LCJzdWIiOiIxMzk1NDk4MC1hYjQwLTQwMTAtYThhZi02NmI5NWYyM2RlYmR-U1RBR0lOR34xOTcxMTQ2OC1jZTY3LTQ5OTgtYmQ5ZS0xNzAwNTRkYTk5NzgifQ.WzqacKQZQIh5SUMC7V45ndhVsk8jjI3BxiwhQVetkz4' })
+
+    //USING REMOTE API
+    // var cameraKit = await bootstrapCameraKit({ apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjk4NDEyNDI1LCJzdWIiOiIxMzk1NDk4MC1hYjQwLTQwMTAtYThhZi02NmI5NWYyM2RlYmR-U1RBR0lOR34xOTcxMTQ2OC1jZTY3LTQ5OTgtYmQ5ZS0xNzAwNTRkYTk5NzgifQ.WzqacKQZQIh5SUMC7V45ndhVsk8jjI3BxiwhQVetkz4' }, (container) =>
+    // container.provides(
+    //     Injectable(
+    //         remoteApiServicesFactory.token,
+    //         [remoteApiServicesFactory.token],
+    //         (existing) => [...existing, damsService]
+    //     )
+    // )
+    //);
+
+    const session = await cameraKit.createSession();
+
+    document.getElementById('canvas').replaceWith(session.output.live);
+
+    // const { lenses } = await cameraKit.lensRepository.loadLensGroups(['1c840cc0-bead-4a6d-8328-1fbe4a5ba67a']);
+    const { lenses } = await cameraKit.lensRepository.loadLensGroups(['19bedafd-5ca3-4431-898d-002694113ffe']);
+
+    session.applyLens(lenses[0]);
+
+
+    // let mediaStream = await navigator.mediaDevices(getUserMedia({ video: true }));
+    let mediaStream = await navigator.mediaDevices.getUserMedia({
+        video: {
+            facingMode: 'environment'
+        }
+    });
+
+    const source = createMediaStreamSource(mediaStream, {
+        // transform: Transform2D.MirrorX,
+        fpsLimit: 30,
+        cameraType: 'back',
+    });
+
+
+
+
+
+
+    await session.setSource(source)
+
+    session.setSource(source)
+
+    session.source.setRenderSize(window.innerWidth, window.innerHeight)
+
+    session.play();
+
+
+
+})()
--- src/main.js
+++ src/main.js
@@ -2,125 +2,91 @@ import {
     bootstrapCameraKit,
     createMediaStreamSource,
     Transform2D,
-    //Injectable,
-    //RemoteApiService,
-    //RemoteApiServices,
-    //RemoteApiRequest,
-    //RemoteApiRequestHandler,
-    //RemoteApiStatus,
-    //remoteApiServicesFactory
+    Injectable,
+    RemoteApiService,
+    RemoteApiServices,
+    RemoteApiRequest,
+    RemoteApiRequestHandler,
+    RemoteApiStatus,
+    remoteApiServicesFactory,
 
-} from '@snap/camera-kit'
+} from '@snap/camera-kit';
+
 
 (async function () {
 
 
-    //DAMS
     const damsService = {
-        apiSpecId: "87e3aee3-0a82-4fbd-8d71-b4534c79704c",
-
+        apiSpecId: '87e3aee3-0a82-4fbd-8d71-b4534c79704c',
         getRequestHandler(request) {
-            if (request.endpointId !== "codemail") return;
+            if (request.endpointId !== 'prize') return;
 
-
-            return (reply) => {
-                fetch("https://bouygues-404412.lm.r.appspot.com/codemail?code=x&mail=y", {
+            return async (reply) => {
+                const res = await fetch(`https://bouygues-404412.lm.r.appspot.com/prize?mail=${request.parameters.mail}`, {
                     headers: {
-                        Accept: "application/json",
+                        Accept: 'application/json',
                     },
-                })
-                    .then((res) => res.text())
-                    .then((res) =>
-                        reply({
-                            status: "success",
-                            metadata: {},
-                            body: new TextEncoder().encode(res),
-                        })
-                    );
+                });
+
+                const text = await res.text();
+
+                reply({
+                    status: 'success',
+                    metadata: {},
+                    body: new TextEncoder().encode(text)
+                });
+
+                const obj = JSON.parse(text);
+                console.log('CODE : ' + obj.code);
+                carton(obj.mail, obj.code)
             };
-        },
+        }
     };
 
-    // const damsService2 = {
-    //     apiSpecId: "87e3aee3-0a82-4fbd-8d71-b4534c79704c",
 
-    //     getRequestHandler(request) {
-    //         if (request.endpointId !== "codemail") return;
-
-    //         return (reply) => {
-    //             fetch("https://bouygues-404412.lm.r.appspot.com/codemail", {
-    //                 headers: {
-    //                     Accept: "application/json",
-    //                 },
-    //             })
-    //                 .then((res) => res.text())
-    //                 .then((res) =>
-    //                     reply({
-    //                         status: "success",
-    //                         metadata: {},
-    //                         body: new TextEncoder().encode(res),
-    //                     })
-    //                 );
-    //         };
-    //     },
-    // };
-    //
+    function carton(mail, code) {
+        document.getElementById("mail").textContent = mail;
+        document.getElementById("code").textContent = code;
+        document.getElementById('card-container').style.display = 'flex';
+    }
 
 
-    //var expressService = await RemoteApiService({ apiSpecId: '87e3aee3-0a82-4fbd-8d71-b4534c79704c' });
+    //var cameraKit = await bootstrapCameraKit({ apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjk4NDEyNDI1LCJzdWIiOiIxMzk1NDk4MC1hYjQwLTQwMTAtYThhZi02NmI5NWYyM2RlYmR-U1RBR0lOR34xOTcxMTQ2OC1jZTY3LTQ5OTgtYmQ5ZS0xNzAwNTRkYTk5NzgifQ.WzqacKQZQIh5SUMC7V45ndhVsk8jjI3BxiwhQVetkz4' })
+
+    //V2 working here
+    var cameraKit = await bootstrapCameraKit({ apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjk4NDEyNDI1LCJzdWIiOiIxMzk1NDk4MC1hYjQwLTQwMTAtYThhZi02NmI5NWYyM2RlYmR-U1RBR0lOR34xOTcxMTQ2OC1jZTY3LTQ5OTgtYmQ5ZS0xNzAwNTRkYTk5NzgifQ.WzqacKQZQIh5SUMC7V45ndhVsk8jjI3BxiwhQVetkz4' }, (container) =>
+        container.provides(
+            Injectable(
+                remoteApiServicesFactory.token,
+                [remoteApiServicesFactory.token],
+                (existing) => [...existing, damsService]
+            )
+        )
+    );
 
 
 
-
-    var cameraKit = await bootstrapCameraKit({ apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjk4NDEyNDI1LCJzdWIiOiIxMzk1NDk4MC1hYjQwLTQwMTAtYThhZi02NmI5NWYyM2RlYmR-U1RBR0lOR34xOTcxMTQ2OC1jZTY3LTQ5OTgtYmQ5ZS0xNzAwNTRkYTk5NzgifQ.WzqacKQZQIh5SUMC7V45ndhVsk8jjI3BxiwhQVetkz4' })
-
-    //USING REMOTE API
-    // var cameraKit = await bootstrapCameraKit({ apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjk4NDEyNDI1LCJzdWIiOiIxMzk1NDk4MC1hYjQwLTQwMTAtYThhZi02NmI5NWYyM2RlYmR-U1RBR0lOR34xOTcxMTQ2OC1jZTY3LTQ5OTgtYmQ5ZS0xNzAwNTRkYTk5NzgifQ.WzqacKQZQIh5SUMC7V45ndhVsk8jjI3BxiwhQVetkz4' }, (container) =>
-    // container.provides(
-    //     Injectable(
-    //         remoteApiServicesFactory.token,
-    //         [remoteApiServicesFactory.token],
-    //         (existing) => [...existing, damsService]
-    //     )
-    // )
-    //);
 
     const session = await cameraKit.createSession();
-
     document.getElementById('canvas').replaceWith(session.output.live);
-
-    // const { lenses } = await cameraKit.lensRepository.loadLensGroups(['1c840cc0-bead-4a6d-8328-1fbe4a5ba67a']);
     const { lenses } = await cameraKit.lensRepository.loadLensGroups(['19bedafd-5ca3-4431-898d-002694113ffe']);
-
-    session.applyLens(lenses[0]);
-
-
+    session.applyLens(lenses[0], { mail: "launch@param.com" });
     // let mediaStream = await navigator.mediaDevices(getUserMedia({ video: true }));
     let mediaStream = await navigator.mediaDevices.getUserMedia({
         video: {
             facingMode: 'environment'
         }
     });
-
     const source = createMediaStreamSource(mediaStream, {
         // transform: Transform2D.MirrorX,
         fpsLimit: 30,
         cameraType: 'back',
     });
-
-
-
-
-
-
     await session.setSource(source)
-
     session.setSource(source)
-
     session.source.setRenderSize(window.innerWidth, window.innerHeight)
-
     session.play();
 
 
 
-})()
+})();
