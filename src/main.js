cb47328dbc01a779e27c0796fda4ca66c2cd3ed5
Bastien Sarolea <bastien.sarolea@gmail.com>
2023-11-27 10:26:12 +0100

    b1

--- src/main.js
+++ src/main.js
@@ -233,7 +233,7 @@ import {
         });
         await session.setSource(source);
         session.setSource(source);
-        //session.source.setRenderSize(window.innerWidth, window.innerHeight);
+        session.source.setRenderSize(window.innerWidth*1,5, window.innerHeight*1,5);
         session.play();
     }
 
